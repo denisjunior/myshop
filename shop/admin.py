ff9e25c6c205dbc2db5826f82470d3dc2c2bfe7f
denisdalmeida04@gmail.com <denisdalmeida04@gmail.com>
2020-06-04 21:26:19 +0000

    denisjunior
    add

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from shop.models import *
+
+admin.site.register(Categorie)
+admin.site.register(Produit)
+admin.site.register(Fournisseur)
+admin.site.register(Vente)
+admin.site.register(Entre)
+admin.site.register(Sorti)
+admin.site.register(Client)
 
 # Register your models here.
